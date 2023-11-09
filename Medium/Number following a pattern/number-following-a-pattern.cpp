d6d791880677dccdb792cc00deef48d5570d2c72
Avinash <86429000+akj2002276@users.noreply.github.com>
2023-11-10 00:05:41 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number following a pattern/number-following-a-pattern.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string printMinNumberForPattern(string S){
+        // code here 
+        int n=S.size();
+        string ans(n+1,'0');
+        int nextnum=1;
+        int countD=0;
+        
+        for(int i = 0; i < n; i++) 
+        {
+            
+            if(S[i] == 'I') 
+            {
+                if(countD)
+                {
+                int k=i-countD;
+                int num=nextnum;
+                while(k<=i)
+                {
+                    ans[k++]='0'+num--;
+                }
+                countD=0;
+                }
+                else ans[i]='0'+nextnum;
+               
+            } 
+            else countD++;
+            
+            nextnum++;
+        }
+        
+        if(S[n-1]=='I') ans[n]=nextnum+'0';
+        else
+        {
+                int k=n-countD;
+                int num=nextnum;
+                while(k<=n)
+                {
+                    ans[k++]='0'+num--;
+                }
+                countD=0;
+                
+        }
+        
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.printMinNumberForPattern(S) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
